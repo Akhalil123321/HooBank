ce2d89638d4239991f1b7b3b2dbbe97d559e0862
Ahmed khalil <ahmed.mohamed.khalil1998@gmail.com>
2022-08-25 20:45:47 +0200

    add animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { motion } from "framer-motion"
 import robot from '../../images/robot.png'
+import card1 from '../../images/card1.webp'
+import card2 from '../../images/card2.webp'
+import card3 from '../../images/card3.webp'
 import './hero.css'
 const Hero = () => {
+    const transition1 = {type: 'spring', duration: 1}
+    const transition2 = {type: 'spring', duration: 2, delay:.5}
+    const transition3 = {type: 'spring', duration: 2, delay:.6}
+    const transition4 = {type: 'spring', duration: 2, delay:.9}
+    const transition5 = {
+                        duration: 2,
+                        ease: "easeInOut",
+                        times: [0, 0.2, 0.5, 0.8, 1],
+                        delay : 1.2
+    }
+    const animate5 = {
+                        opacity: [0 , 1, 1, 1, 1],
+                        scale: [1, 1.3, 1, 1, 1],
+                        rotate: [0, 0, 180, 180, 0],
+                        borderRadius: ["50%", "0%", "50%","0%", "50%"]
+    }
     return (
         <>
         <div className='hero-cont'>
@@ -23,10 +43,53 @@ const Hero = () => {
                     </div>
             </div>
             <div className="right-side">
-                <img src={robot} alt="" className='h-r-s-image'/>
-                <div className='ball ball-one'></div>
-                <div className='ball ball-two'></div>
-                <div className='ball ball-three'></div>
+                <motion.img 
+                src={robot} 
+                alt="" 
+                className='h-r-s-image'
+                initial={{right:'-10rem', opacity: 0}}
+                whileInView={{right:'0rem', opacity: 1}}
+                transition={transition1}
+                />
+                <motion.img 
+                src={card1} 
+                alt="" 
+                className='h-r-s-cards h-r-s-card1'
+                initial={{opacity:0, y:10}}
+                whileInView={{opacity:1, y:0}}
+                transition={transition4}
+                />
+                <motion.img 
+                src={card2}
+                alt="" 
+                className='h-r-s-cards h-r-s-card2'
+                initial={{opacity:0, y:50}}
+                whileInView={{opacity:1, y:0}}
+                transition={transition3}
+                />
+                <motion.img 
+                src={card3} 
+                alt="" 
+                className='h-r-s-cards h-r-s-card3'
+                initial={{opacity:0, y:10}}
+                whileInView={{opacity:1, y:0}}
+                transition={transition2}
+                />
+                <motion.div 
+                className='ball ball-one'
+                animate={animate5}
+                transition={transition5}
+                ></motion.div>
+                <motion.div 
+                className='ball ball-two'
+                animate={animate5}
+                transition={transition5}
+                ></motion.div>
+                <motion.div 
+                className='ball ball-three'
+                animate={animate5}
+                transition={transition5}
+                ></motion.div>
             </div>
         </div>
             <div className='counter'>
